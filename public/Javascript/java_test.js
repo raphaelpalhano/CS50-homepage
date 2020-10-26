9cf80a20404bbfe20b3f98b7355cdb01b9203430
Raphael Angel <raphael-angel@hotmail.com>
2020-10-26 17:59:30 -0300

    Projeto finalizado

---
+++ public/Javascript/java_test.js
@@ -0,0 +1,29 @@
+document.querySelector('select').onchange = function() {
+       document.querySelector('p').style.fontSize = this.value;
+ 
+      }
+
+function cumprimento()
+
+{        
+       alert("Seja bem vindo a sessão leitura.")
+       alert("Meu nome é Raphael Angel, estou aqui para lhe indicar duas leituras maravilhosas!.")
+       alert("Você irá ver dois livros que contextualizam a minha evolução e sabedoria.")
+}
+
+function serie()
+
+{        
+       alert("Seja bem vindo a sessão das séries.")
+       alert("Meu nome é Raphael Angel, estou aqui para lhe indicar duas séries maravilhosas!.")
+       alert("A primeira eu assisti sem parar. A segunda nem se fala!")
+}
+
+
+function academia()
+
+{        
+       alert("Seja bem vindo a sessão da musculação.")
+       alert("Meu nome é Raphael Angel, estou aqui para lhe indicar dois exercícios potenciais.")
+       alert("Supino com Barra e Afundo. Veja os seus benefícios!")
+}
